a7a4bfecff5123e02f07ba77716fba5e31280cf2
Keerthi <mkeerthilakshmi@gmail.com>
2026-04-29 16:25:31 +0530

    first commit

--- student_project/settings.py
+++ student_project/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-5ng+1l^w)eq&snc)h6et60b+(xn!94vis@s8xnn&sx%ylpj)!&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['*']
+
+MIDDLEWARE = [
+  
+]
 
 # Application definition
 
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
